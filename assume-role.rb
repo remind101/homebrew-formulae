7b3e767fc328e2e867abcd44b280371ae68972d4
Eric J. Holmes <eric@ejholmes.net>
2016-01-26 10:25:52 +0700

    Add assume-role.

---
+++ assume-role.rb
@@ -0,0 +1,13 @@
+class AssumeRole < Formula
+  desc "Easily assume AWS roles in your terminal"
+  homepage "https://github.com/remind101/assume-role"
+  url "https://github.com/remind101/assume-role/releases/download/0.1.0/assume-role"
+  version "0.1.0"
+  sha256 "837b2035e95b6fc8ced30b20d9b3231877066686ba26114ca2843385b10bbb99"
+
+  def install
+  end
+
+  test do
+  end
+end
