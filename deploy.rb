04bc0971f815476ad73d6dc9df8e7587b0175284
Benjamin Guillet <benjamin.guillet@gmail.com>
2016-01-26 18:35:10 -0800

    Rename release

--- deploy.rb
+++ deploy.rb
@@ -1,12 +1,12 @@
 class Deploy < Formula
   desc "CLI for GitHub Deployments"
   homepage "https://github.com/remind101/deploy"
-  url "https://github.com/remind101/deploy/releases/download/v0.0.5/deploy_0.0.5_darwin_amd64"
+  url "https://github.com/remind101/deploy/releases/download/v0.0.5/deploy-Darwin-x86_64"
   version "0.0.5"
   sha256 "f01174700bda611f6baa16954a437ef9efc1dbcbdc9a076306754c77e888a1ca"
 
   def install
-    bin.install "deploy_0.0.5_darwin_amd64" => "deploy"
+    bin.install "deploy-Darwin-x86_64" => "deploy"
   end
 
   test do
