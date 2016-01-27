b77c45432b640e9e97e332ba05210643131b0873
Benjamin Guillet <benjamin.guillet@gmail.com>
2016-01-26 18:25:43 -0800

    Update deploy to v0.0.5

--- deploy.rb
+++ deploy.rb
@@ -1,12 +1,12 @@
 class Deploy < Formula
   desc "CLI for GitHub Deployments"
   homepage "https://github.com/remind101/deploy"
-  url "https://github.com/remind101/deploy/releases/download/v0.0.4/darwin_amd64_deploy"
-  version "0.4.0"
-  sha256 "db3e7b7181cdf3e57b497fc5ea6ff590ca75bde72b8d301f05d60ce00fe3f77b"
+  url "https://github.com/remind101/deploy/releases/download/v0.0.5/deploy_0.0.5_darwin_amd64"
+  version "0.0.5"
+  sha256 "f01174700bda611f6baa16954a437ef9efc1dbcbdc9a076306754c77e888a1ca"
 
   def install
-    bin.install "darwin_amd64_deploy" => "deploy"
+    bin.install "deploy_0.0.5_darwin_amd64" => "deploy"
   end
 
   test do
