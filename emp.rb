d5bbe545eb55c8ee086f354b1aa92d453b2f0c6f
Eric J. Holmes <eric@ejholmes.net>
2016-08-22 20:11:22 -0700

    Emp 0.11.0

--- emp.rb
+++ emp.rb
@@ -1,9 +1,9 @@
 class Emp < Formula
   desc "CLI for Empire"
   homepage "https://github.com/remind101/empire"
-  url "https://github.com/remind101/empire/releases/download/v0.10.1/emp-Darwin-x86_64"
+  url "https://github.com/remind101/empire/releases/download/v0.11.0/emp-Darwin-x86_64"
   version "0.10.1"
-  sha256 "23580df651606e6d45ac92818d6639438e1569025160e001b259144dd5735f3e"
+  sha256 "15f3889c6447c3200fb2ca816508069a129de535da5ad3c53a0463664f8c7a14<Paste>"
 
   def install
     bin.install "emp-Darwin-x86_64" => "emp"
