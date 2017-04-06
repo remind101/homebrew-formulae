6a34bdb45127df5e363198af6c511bd51fe0987d
Eric J. Holmes <eric@ejholmes.net>
2017-04-06 16:06:25 -0700

    Update assume-role and emp

--- assume-role.rb
+++ assume-role.rb
@@ -1,9 +1,9 @@
 class AssumeRole < Formula
   desc "Easily assume AWS roles in your terminal"
   homepage "https://github.com/remind101/assume-role"
-  url "https://github.com/remind101/assume-role/releases/download/0.1.3/assume-role"
-  version "0.1.3"
-  sha256 "46787865a93feca0061db3afe63212dbbaaabace36ac84fb1dc185a6e56b8f7c"
+  url "https://github.com/remind101/assume-role/releases/download/0.2.0/assume-role"
+  version "0.2.0"
+  sha256 "44a7eac791207db3c2886ac4cb85f1d12cedaa92c28c29fc3c3117b83ca741c4"
 
   def install
     bin.install "assume-role"
--- emp.rb
+++ emp.rb
@@ -1,9 +1,9 @@
 class Emp < Formula
   desc "CLI for Empire"
   homepage "https://github.com/remind101/empire"
-  url "https://github.com/remind101/empire/releases/download/v0.10.1/emp-Darwin-x86_64"
-  version "0.10.1"
-  sha256 "23580df651606e6d45ac92818d6639438e1569025160e001b259144dd5735f3e"
+  url "https://github.com/remind101/empire/releases/download/v0.12.0/emp-Darwin-x86_64"
+  version "0.12.0"
+  sha256 "9f96e3f09ea6ce8d033a878066dbaa6fa6952b9b48686eccdd6d3bf895b5aa11"
 
   def install
     bin.install "emp-Darwin-x86_64" => "emp"
