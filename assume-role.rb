3b525c5e40e0005e440d7937ed70e27d5af4107d
Eric J. Holmes <eric@ejholmes.net>
2017-04-22 17:31:27 -0700

    Update assume-role

--- assume-role.rb
+++ assume-role.rb
@@ -1,9 +1,9 @@
 class AssumeRole < Formula
   desc "Easily assume AWS roles in your terminal"
   homepage "https://github.com/remind101/assume-role"
-  url "https://github.com/remind101/assume-role/releases/download/0.2.0/assume-role"
-  version "0.2.0"
-  sha256 "44a7eac791207db3c2886ac4cb85f1d12cedaa92c28c29fc3c3117b83ca741c4"
+  url "https://github.com/remind101/assume-role/releases/download/0.3.0/assume-role"
+  version "0.3.0"
+  sha256 "bc668db75c23c5634d7b7f8e1c6dbbb5483557977180dc7d776c4a0344b2800d"
 
   def install
     bin.install "assume-role"
